5e0b7652268f063c755c400446fadd40149b7376
archana <archanatshinde@gmail.com>
2024-08-18 08:09:12 +1000

    fix for locator

--- tests/record_demo.spec.ts
+++ tests/record_demo.spec.ts
@@ -1,16 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test ('My record demo test',async({page}) => {
-  
+  // recording test with code gen
   await page.goto('https://www.saucedemo.com/')
   await page.locator('[data-test="username"]').click()
   await page.locator('[data-test="username"]').fill('standard_user')
   await page.locator('[data-test="password"]').click()
   await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button1"]').click();
+  //login action with click on logn button
+  await page.locator('[data-test="login-button"]').click();
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   await page.locator('[data-test="shopping-cart-link"]').click();
-  
+  //close page
   await page.close();
  
 
